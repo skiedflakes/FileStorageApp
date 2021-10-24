550a71946d2d83c746257c63bedde68ffb959bc8
skiedflakes <gagito0594@gmail.com>
2021-10-24 18:26:12 +0800

    updates
    updates

--- app/src/main/java/com/example/filestorage_app/Login.java
+++ app/src/main/java/com/example/filestorage_app/Login.java
@@ -1,11 +1,20 @@
 package com.example.filestorage_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -39,10 +48,69 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 String input_username = username.getText().toString();
                 String input_password = password.getText().toString();
-                api_login_function(input_username,input_password);
+//                api_login_function(input_username,input_password);
+                haveStoragePermission();
+
             }
         });
     }
+    public  boolean haveStoragePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.e("Permission error","You have permission");
+                String testurl = "http://localhost/prototype_project_board/prototype_board/uploads/document.pdf";
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(testurl));
+                String title = URLUtil.guessFileName(testurl,null,null);
+                request.setTitle(title);
+                request.setDescription("download files");
+                String cookie = CookieManager.getInstance().getCookie(testurl);
+                request.addRequestHeader("cookie",cookie);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,title);
+
+                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                downloadManager.enqueue(request);
+                Toast.makeText(Login.this, "Downloaded started", Toast.LENGTH_SHORT).show();
+
+                return true;
+            } else {
+
+                Log.e("Permission error","You have asked for permission");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //you dont need to worry about these stuff below api level 23
+            Log.e("Permission error","You already have the permission");
+            return true;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            //you have the permission now.
+            String testurl = "http://localhost/prototype_project_board/prototype_board/uploads/document.pdf";
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(testurl));
+            String title = URLUtil.guessFileName(testurl,null,null);
+            request.setTitle(title);
+            request.setDescription("download files");
+            String cookie = CookieManager.getInstance().getCookie(testurl);
+            request.addRequestHeader("cookie",cookie);
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,title);
+
+            DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            downloadManager.enqueue(request);
+            Toast.makeText(Login.this, "Downloaded started", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 
     void api_login_function(final String input_username, final String input_password) {
         String URL =  getString(R.string.URL)+"login.php";
@@ -93,4 +161,6 @@ public class Login extends AppCompatActivity {
         AppController.getInstance().addToRequestQueue(stringRequest);
         AppController.getInstance().setVolleyDuration(stringRequest);
     }
+
+
 }
